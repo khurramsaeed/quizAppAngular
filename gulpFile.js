40728f3f8235f0d3074abaffb81f2e92fd3f5532
Khurram Saeed Malik <khurramsaeedmalik@Khurrams-iMac.lan>
2017-12-22 00:21:57 +0100

    gulp build&buildLibs added

---
+++ gulpFile.js
@@ -0,0 +1,58 @@
+'use strict';
+
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var angularFileSort = require('gulp-angular-filesort');
+var templateCache = require('gulp-angular-templateCache');
+var ngAnnotate = require('gulp-ng-annotate');
+var mainBowerFiles = require('main-bower-files');
+var gulpUglify = require('gulp-uglify');
+
+
+gulp.task('buildLibs', function () {
+    return gulp.src(mainBowerFiles())
+      .pipe(gulp.dest('libs'));
+});
+
+gulp.task('libs', function () {
+  return gulp.src('libs/*.js')
+      .pipe(angularFileSort())
+      .pipe(concat('libs.js'))
+      .pipe(gulpUglify())
+      .pipe(gulp.dest('dist/src'));
+});
+
+gulp.task('appModules', function () {
+  return gulp.src('app/js/**/*.js')
+      .pipe(ngAnnotate())
+      .pipe(angularFileSort())
+      .pipe(concat('script.js'))
+      .pipe(gulp.dest('dist/src'));
+});
+
+gulp.task('css', function () {
+    return gulp.src('app/css/*.css')
+      .pipe(concat('styles.css'))
+      .pipe(gulp.dest('dist/src'));
+});
+
+gulp.task('templates', function () {
+    return gulp.src('app/templates/**/*.html')
+      .pipe(templateCache('template.js', {
+        module: 'quizApp',
+        root: 'app/templates/'
+      }))
+      .pipe(gulp.dest('dist/src'));
+});
+
+gulp.task('index', function () {
+    return gulp.src('app/index.html')
+      .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('appMain', function() {
+    return gulp.src('app/app.js')
+    .pipe(gulp.dest('dist/'))
+});
+
+gulp.task('build', ['index', 'templates', 'css', 'libs', 'appModules', 'appMain']);
